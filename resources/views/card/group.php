86efdcc2aff1dff8df7e68f33ce5c69646db439a
TOBENTOch <info@tobento.ch>
2025-06-23 16:52:45 +0200

    changed cards class

--- resources/views/card/group.php
+++ resources/views/card/group.php
@@ -4,7 +4,7 @@
         <div class="card-head"><?= $view->esc($card->title()) ?></div>
     <?php } ?>
     <div class="card-body">
-        <div class="cards-small">
+        <div class="cards">
             <?php
             foreach($card->cards() as $gcard) {
                 echo $gcard->render();
